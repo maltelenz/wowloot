a8c81461b698e235a323546e61541c9d6f623910
Malte Lenz <malte.lenz@gmail.com>
2012-04-25 20:10:24 +0200

    updated styles

--- calcloot/forms.py
+++ calcloot/forms.py
@@ -10,6 +10,10 @@ class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         exclude = ('calculation', 'benefactors',)
+        widgets = {
+            'amount': forms.TextInput(attrs={'size': '8'}),
+            'name': forms.TextInput(attrs={'size': '25'}),
+            }
 
 class AddPersonForm(forms.Form):
     name = forms.CharField(max_length = 200)
